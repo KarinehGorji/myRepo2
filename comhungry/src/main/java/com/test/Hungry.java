99eb59edc6238698d8e3e8b8ceeaa9936134f2d8
Karine Gorji <karineh.gorji@gmail.com>
2018-05-03 15:45:08 +0400

    second commit

---
+++ comhungry/src/main/java/com/test/Hungry.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import javax.sound.midi.SysexMessage;
+
+public class Hungry {
+
+    public static void main(String args[]){
+
+        System.out.println("I am hungry");
+    }
+}
