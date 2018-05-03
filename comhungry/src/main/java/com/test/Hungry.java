3f955b41c8b6d2e3109e8ca05420f4b5d5ace33d
Karine Gorji <karineh.gorji@gmail.com>
2018-05-03 16:27:10 +0400

    added hungry prompt

--- comhungry/src/main/java/com/test/Hungry.java
+++ comhungry/src/main/java/com/test/Hungry.java
@@ -1,11 +1,21 @@
 package com.test;
 
 import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
 
 public class Hungry {
 
     public static void main(String args[]){
 
-        System.out.println("I am hungry");
+        System.out.println("are you hungry?");
+
+        Scanner in = new Scanner(System.in);
+
+        String input = in.next();
+
+        if(input.equals("yes"))
+            System.out.println("eat something");
+        else
+            System.out.println("thats great");
     }
 }
